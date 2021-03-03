1bdf57f11dd8e05533a8755796eb80f1b75a23d9
Aaron Tabor <aarontabor50@gmail.com>
2021-03-03 15:22:32 -0400

    Display author of posts in both PostList and SinglePost components.

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { PostAuthor } from './PostAuthor'
+
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
@@ -14,6 +16,7 @@ export const PostsList = () => {
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
+      <PostAuthor userId={post.user} />
     </article>
   ))
 
